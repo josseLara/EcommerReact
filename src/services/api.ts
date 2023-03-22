db46d80abd2efc3b5d94364d24a1ce688e013f25
José Lara <josselara1@gmail.com>
2023-03-22 14:33:17 -0300

    .

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import { ProductInterfece } from "../features/productSlice";
 export const getDataAPI =  (dispatch: any, setProductState: any): ProductInterfece[] => {
       let dataProd: ProductInterfece[] = []
       let token = localStorage.getItem('token');
-      
+
       axios.post(`${process.env.REACT_APP_URL_PRODUCT}`, { token })
       .then((dataProduct) => {
             dataProd = dataProduct.data
@@ -12,15 +12,5 @@ export const getDataAPI =  (dispatch: any, setProductState: any): ProductInterfe
             return dataProd;
       })
      
-      // dataProd = data
-      // dispatch(setProductState(data))
-      //      fetch(`${process.env.REACT_APP_URL_PRODUCT}`)
-      //      .then(response => response.json())
-      //      .then(data => { 
-      //            console.log(data)    
-      //            dataProd = data
-      //            dispatch(setProductState(data))
-
-      //      });
       return dataProd
 }
