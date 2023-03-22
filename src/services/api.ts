e34d14cb6853f344cf78c5517ff36f24c575f05d
José Lara <josselara1@gmail.com>
2023-03-22 12:30:44 -0300

    .

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import { ProductInterfece } from "../features/productSlice";
 
 export const getDataAPI = (dispatch:any,setProductState:any):ProductInterfece[]=>{
      let dataProd:ProductInterfece[] = []
-     fetch('http://localhost:3001/product')
+     fetch(`${process.env.REACT_APP_URL_PRODUCT}`)
      .then(response => response.json())
      .then(data => { 
            console.log(data)    
