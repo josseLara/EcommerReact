140f130f74f4b459a044a52e1f9c2af6009ed0b4
José Lara <josselara1@gmail.com>
2023-03-24 20:18:45 -0300

    a

--- src/Page/Login/index.tsx
+++ src/Page/Login/index.tsx
@@ -55,7 +55,7 @@ function Login() {
           }
      }
      useEffect(() => {
-          if (localStorage.getItem("token") && !boolToken){
+          if (!boolToken){
                try {
                     setShowLoading(true);
                     getTokenSave(dispatch, setProfile)
